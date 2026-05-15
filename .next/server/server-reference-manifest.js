2b13a727037050455045a4cd4de66f6b2a47d455
Birthday Portal <birthday@portal.com>
2026-05-15 06:18:27 +0100

    Fix component resolution and dependencies (code only)

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DyeHrMfsxyoBjqLv1yEuWbuQwXyhRBT5vxXCJRdEMu8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"F37te5xy+1aRozfwkBpxEcs45/l4hI0Xd/EiNEtB650=\"\n}"
